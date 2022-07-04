bf60ff8e14c54aa76ccdc7a2751f4d0679c44068
冯凯超 <kaichao.feng@w-group.com.cn>
2022-07-04 18:52:41 +0800

    feat: 更改为类组件

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,67 +1,64 @@
-import { useState } from 'react';
+import React, { Component } from 'react';
 import { Divider, Radio, Table } from 'antd';
-import { Link, useNavigate } from 'react-router-dom'
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    render: (text) => <a href="https://github.com/" target="__blank">{text}</a>,
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-  },
-];
-const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-  },
-  {
-    key: '4',
-    name: 'Disabled User',
-    age: 99,
-    address: 'Sidney No. 1 Lake Park',
-  },
-]; // rowSelection object indicates the need for row selection
+import { Link } from 'react-router-dom';
 
-const rowSelection = {
-  onChange: (selectedRowKeys, selectedRows) => {
+import WithRouter from '@/hooks/WithRouter';
+
+class index extends Component {
+  state = {
+    selectionType: 'checkbox',
+    columns: [
+      {
+        title: 'Name',
+        dataIndex: 'name',
+        render: (text) => <a href="https://github.com/" target="__blank">{text}</a>,
+      },
+      {
+        title: 'Age',
+        dataIndex: 'age',
+      },
+      {
+        title: 'Address',
+        dataIndex: 'address',
+      },
+    ],
+    data: [
+      {
+        key: '1',
+        name: 'John Brown',
+        age: 32,
+        address: 'New York No. 1 Lake Park',
+      },
+      {
+        key: '2',
+        name: 'Jim Green',
+        age: 42,
+        address: 'London No. 1 Lake Park',
+      },
+      {
+        key: '3',
+        name: 'Joe Black',
+        age: 32,
+        address: 'Sidney No. 1 Lake Park',
+      },
+      {
+        key: '4',
+        name: 'Disabled User',
+        age: 99,
+        address: 'Sidney No. 1 Lake Park',
+      },
+    ]
+  }
+  onChange = (selectedRowKeys, selectedRows) => {
     console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
-  },
-  getCheckboxProps: (record) => ({
-    disabled: record.name === 'Disabled User',
-    // Column configuration not to be checked
-    name: record.name,
-  }),
-};
-
-const App = (props) => {
-  const [selectionType, setSelectionType] = useState('checkbox');
-  const navigate = useNavigate()
-  return (
-    <div>
-      <Radio.Group
+  }
+  render(){
+    const { selectionType, columns, data} = this.state;
+    return (
+      <div>
+        <Radio.Group
         onChange={({ target: { value } }) => {
-          setSelectionType(value);
+          this.setState({selectionType: value});
         }}
         value={selectionType}
       >
@@ -69,19 +66,20 @@ const App = (props) => {
         <Radio value="radio">radio</Radio>
       </Radio.Group>
       <Link to={{pathname: 'memberInfo', search: 'name=123'}}>memberInfo</Link>
-      <b onClick={()=> navigate('/memberInfo', { state: {name: 'fkc'} })}>memberInfo</b>
+      {/* <b onClick={()=> navigate('/order', { state: {name: 'fkc'} })}>去订单</b> */}
       <Divider />
 
       <Table
         rowSelection={{
           type: selectionType,
-          ...rowSelection,
+          onChange: this.onChange
         }}
         columns={columns}
         dataSource={data}
       />
-    </div>
-  );
-};
+      </div>
+    )
+  }
+}
 
-export default App;
+export default WithRouter(index);
