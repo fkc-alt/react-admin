c6eac6a414e7d742af42440ccd7401e1cd22d21f
冯凯超 <kaichao.feng@w-group.com.cn>
2022-07-06 11:49:23 +0800

    feat: Enable  strict

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ process.env.NODE_ENV === 'development' && require('../mock/index');
 //render to Page
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  // </React.StrictMode>
+  </React.StrictMode>
 )
