48aaa91689f7e001af7316c49a52e2be6e52266d
冯凯超 <kaichao.feng@w-group.com.cn>
2022-07-06 12:40:01 +0800

    fix: 修复已知bug

--- src/hooks/WithRouter.jsx
+++ src/hooks/WithRouter.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 
-import { routes, RootSubmenuKeys as handleRootSubmenuKeys } from "@/router";
-
 // 可能以后要添加case
 function matchRouterKeys(openKeys){
   switch (openKeys.length) {
@@ -11,7 +9,7 @@ function matchRouterKeys(openKeys){
     case 3:
       return [`/${openKeys.slice(1, 2)}`];
     case 4:
-      return [`/${openKeys.slice(1, 2)}`, `/${openKeys.slice(2, 3)}`];
+      return [`/${openKeys.slice(1, 2)}`, `/${openKeys.slice(1, 3).join('/')}`];
     default:
       return [];
   }
@@ -23,16 +21,8 @@ export default function WithRouter(Component) {
   function ComponentWithRouterProp(props) {
     const [ openKeys, setOpenKeys ] = useState([]);
     const [location, navigate, params] = [useLocation(), useNavigate(), useParams()];
-    const RootSubmenuKeys = handleRootSubmenuKeys(routes);
-    function handleOpenChange(openKeys) {
-      const latestOpenKey = openKeys.find(key => openKeys.indexOf(key) === -1);
-      if (RootSubmenuKeys.indexOf(latestOpenKey) === -1) {
-        setOpenKeys(openKeys);
-      } else {
-        setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
-      }
-    }
-    
+    const handleOpenChange = (openKeys) => setOpenKeys(openKeys);
+
     useEffect(() => {
       setOpenKeys(matchRouterKeys(location.pathname.split("/")));
     }, [location]);
