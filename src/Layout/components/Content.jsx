8cdb5c8f04abf1bfa53a3268741a9644350db1f1
冯凯超 <kaichao.feng@w-group.com.cn>
2022-07-15 13:45:00 +0800

    prettier

--- src/Layout/components/Content.jsx
+++ src/Layout/components/Content.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Layout } from "antd";
 
-import { MapRoutes as RouterView} from "@/router";
+import { MapRoutes as RouterView } from "@/router";
 import content from "../styles/content.module.scss";
 
 const { Content } = Layout;
@@ -9,7 +9,7 @@ export default class index extends Component {
   render() {
     return (
       <Content className={content.container}>
-        <RouterView {...this.props}/>
+        <RouterView {...this.props} />
       </Content>
     );
   }
