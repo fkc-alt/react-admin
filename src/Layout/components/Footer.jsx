c994bf35c33cf930d7bb7159a3e5b2d12511f65d
冯凯超 <kaichao.feng@w-group.com.cn>
2022-08-16 17:32:59 +0800

    feat: Footer Component 开发

--- src/Layout/components/Footer.jsx
+++ src/Layout/components/Footer.jsx
@@ -7,7 +7,19 @@ export default class index extends Component {
   render() {
     return (
       <Footer className={footer.container}>
-        <span className={footer.title}>footer</span>
+        <div className={footer.title}>
+          <span>Copyright © 2012-2022 React Admin. All Rights Reserved.</span>
+          <a
+            href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=11000002000001"
+            target="_blank"
+          >
+            <img
+              src="https://www.2345.com/images/index/renzheng_gab.png"
+              alt=""
+            />
+            京公网安备11000002000001号
+          </a>
+        </div>
       </Footer>
     );
   }
