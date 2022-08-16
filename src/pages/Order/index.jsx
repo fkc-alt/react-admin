a77e48b9c3cc9c4f0e575e099507369646f4eb6b
冯凯超 <kaichao.feng@w-group.com.cn>
2022-08-16 15:44:01 +0800

    feat: 测试antd组件

--- src/pages/Order/index.jsx
+++ src/pages/Order/index.jsx
@@ -1,8 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from "react";
+import { Steps } from "antd";
+const { Step } = Steps;
 export default class index extends Component {
+  VNode = createRef()
+  componentDidMount() {
+    console.log(this.VNode);
+  }
   render() {
     return (
-      <div>Order</div>
-    )
+      <div ref={this.VNode}>
+        <Steps size="small" current={1} >
+          <Step title="Finished" />
+          <Step title="In Progress" />
+          <Step title="Waiting" />
+        </Steps>
+        order
+      </div>
+    );
   }
 }
